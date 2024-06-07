d3c8179fd0fef1bfdc7ffa7073fc2d62aff56927
venumadhavi137 <2020WA86853@wilp.bits-pilani.ac.in>
2024-06-07 07:30:29 +0000

    Update JenkinsExample.java

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -2,6 +2,6 @@ public class JenkinsExample
   {
     public static void main(String[] args)
     {
-      System.Out.println("Welcome to Jenkins");
+      System.out.println("Welcome to Jenkins");
         }
   }
